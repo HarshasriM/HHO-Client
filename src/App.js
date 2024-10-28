c6b82d73f9ea8039e5dc6f9449f6f4e1aa03a02b
HarshasriM <medaharsha17@gmail.com>
2024-10-28 12:13:33 +0530

    Setup folder structure, add About page, and configure routes

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import {Routes,Route,Link} from 'react-router-dom';
+import Home from './Pages/Home/HomePage';
+import Event from './Pages/Event/EventPage';
+import Transactions from './Pages/Transaction/TransactionPage';
+import About from './Pages/About/AboutPage';
+import Services from './Pages/Ourservices/OurservicesPage';
+import Login from './Pages/Login/LoginPage';
+import Footer from './components/Footer';
+import Dashbord from './Pages/Dashboard/DashboardPage';
+import { useContext } from 'react';
+import { AppContext } from './context/Context';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import ProfileContainer from './Pages/Profile/ProfilePage';
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+function App() {
+  const{handleClose,open,alertMsg,setAlertMsg} = useContext(AppContext);
+  return (
+    <>
+      <Navbar />
+      {
+        <Snackbar
+        open={open}
+        autoHideDuration={3000} // Auto close after 3 seconds
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }} // Position at the top-right corner
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          {alertMsg}
+        </Alert>
+      </Snackbar>
+      }
+      <Routes>
+        <Route  path='/' element={<Home />}/>
+        
+        <Route path='/events' element={<Event />} />
+        <Route path='/transactions' element={<Transactions />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashbord />} />
+        <Route path='/profile' element={<ProfileContainer />} />
+
+      </Routes>
+    <Footer />
+
+    </>
+  );
+}
+
+export default App;

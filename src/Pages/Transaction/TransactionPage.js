f92ca0db46c55e0cb3d8a22214e60c8c7a58b99a
HarshasriM <medaharsha17@gmail.com>
2025-01-23 20:43:01 +0530

    edited transaction page and added skeleton structure

--- src/Pages/Transaction/TransactionPage.js
+++ src/Pages/Transaction/TransactionPage.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import TransactionLayout from '../../components/Transaction/TransactionLayout';
-
+import TransactionSkeleton from "../../components/Skeletons/TransactionSkeleton"
+import { useState,useEffect } from 'react';
 
 function Transaction() {
+    const [loading,setLoading]=useState(true);
+    useEffect(() => {
+        // Simulate data fetching
+        const timer = setTimeout(() => setLoading(false), 2000); // Adjust the timing as needed
+        return () => clearTimeout(timer);
+      }, []);
     return (
         <div>
+            {loading && <TransactionSkeleton/>}
             <TransactionLayout/>
         </div>
     );
+    // return (
+        
+    //     <div>
+    //         <TransactionLayout/>
+    //     </div>
+    // );
 }
 
 export default Transaction;
